b8ec8d0273f2b258c7194e4df260fc44dc8e024f
AbdulRaufHassan <raufh1093@gmail.com>
2024-07-07 23:52:38 +0500

    created user and video schema

---
+++ src/models/user.js
@@ -0,0 +1,48 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: [true,"Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+  },
+  { timestamp: true }
+);
+
+export const User = mongoose.model("User", userSchema);
